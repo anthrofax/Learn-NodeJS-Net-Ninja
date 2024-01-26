fb88aec538cdf06ccf4bf129386220351c1d075a
anthrofax <afridhoikhsan@gmail.com>
2024-01-26 08:04:43 +0700

    Adding explaination to the things that i haven't understood

--- lat5/app.js
+++ lat5/app.js
@@ -8,23 +8,31 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();
 
 // Connect to MongoDB
-const dbURI = "mongodb+srv://anthrofax:mar777it39@anthrofax.bzf9t83.mongodb.net/node-tutorial";
+const dbURI = "mongodb+srv://anthrofax:cTG8Ojvjm1zd3ygN@anthrofax.bzf9t83.mongodb.net/node-tutorial";
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(res => {
   app.listen(3000,  function() {
     console.log('Siap menerima permintaan.')
     })
 })
-.catch(err => console.log('Halo Salah!'));
+.catch(err => console.log(`Halo Salah, Error Message : ${err}`));
 
 // Register View Engine
+// app.set digunakan untuk mengatur konfigurasi pada app
 app.set("view engine", "ejs");
 // app.set("view", "myview", "ejs"); //Syntax untuk mendaftarkan View Engine, tapi view folder nya memiliki nama yang berbeda (bukan "views")
 
 // Middleware & Static files
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
-app.use(morgan('dev')); //Tidak dijelaskan secara rinci fungsinya di net ninja
+app.use(express.static('public')); //Untuk mendaftarkan folder yang dapat diakses dari frontent (Mengatur folder tertentu menjadi public)
+
+// Hal di atas perlu dilakukan, karena sisi client/frontend tidak dapat mengakses file yang ada di server/backend dengan mudah, jadi file static seperti img, style, tidak dapat diakses di client jika kita tidak mendaftarkannya secara eksplisit menjadi public. Untuk melakukannya kita bisa menggunakan express.static() method
+
+app.use(express.urlencoded({extended: true})); //Untuk melakukan enkode data yang dikirim oleh form pada sisi client/frontend, Memformat data menjadi object dan ditampung pada req.body pada route action yang diatur pada form client nya - Untuk Post Request 
+
+// Jika tidak melakukan hal tersebut, data yang dikirim dari form akan undefined
+
+
+app.use(morgan('dev')); //Untuk melakukan log detail di setiap request yang dilakukan
 
 // -------------------------- NODE EXPRESS ROUTES -------------------------- //
 
@@ -80,7 +88,10 @@ app.get('/', function(req, res) {
 app.get("/about", function (req, res) {
   // res.sendFile("./views/about.html", { root: __dirname });
 
-  res.render("about", { title: "About" });
+  // res.render() digunakan untuk menghandle response dengan cara merendernya.
+  // Parameter 1: file html yang ingin dirender, tulisakan nama filenya tanpa ekstensinya (Otomatis mencari ke folder 'views')
+  // Parameter 2: Untuk mengirimkan data ke file html, (Seperti konsep pengiriman data pada blade route)
+  res.render("about", { title: "Test" });
 });
 
 /*
